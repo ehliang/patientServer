6c5c4957f81e6e98e614653fcaaebb9a5ae75591
Ethan Liang <ethanliang97@gmail.com>
2016-08-30 00:27:50 -0400

    Added source

--- patientServer.go
+++ patientServer.go
@@ -3,15 +3,42 @@ package main
 import (
     "fmt"
     "net/http"
+    "html/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
+type Patient struct {
+    Name string
+   // Email string
+   // Phone string
+    //Passwd string
 }
 
 
+func adminHandler(w http.ResponseWriter, r *http.Request){
+    title := r.URL.Path[len("/admin/"):] 
+    fmt.Fprintf(w, "Hello %s", title)
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request){
+    t, _ := template.ParseFiles("dashboard.html")
+    t.Execute(w, nil)
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request){
+    name := r.FormValue("Name")
+    p := &Patient{Name: name}
+
+    fmt.Fprintf(w, "Hello %s", p.Name)
+
+
+    }
+
+
+
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/admin/", adminHandler)
+    http.HandleFunc("/dashboard/", userHandler)
+    http.HandleFunc("/register/", registerHandler)
     http.ListenAndServe(":8080", nil)
 }
 
